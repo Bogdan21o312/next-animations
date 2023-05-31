5d31787263d77eeacd500ae1e9acb7806a0123da
Bogdan21o312 <lutskiv2010@gmail.com>
2023-05-31 10:16:34 +0300

    Update

---
+++ src/app/(pages)/page.tsx
@@ -0,0 +1,8 @@
+
+export default function Home() {
+  return (
+    <div>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad aliquam at consectetur, distinctio dolores dolorum eum expedita harum hic illum iste nobis nulla optio quia, quidem rerum temporibus velit, voluptates.
+    </div>
+  )
+}
